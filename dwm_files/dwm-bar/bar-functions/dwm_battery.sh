2390670aafda44f734c553d28fdf9fe01f7d5a30
altair-pod <rakib.abir1999@gmail.com>
2021-07-28 21:14:20 +0600

    testing

---
+++ dwm_files/dwm-bar/bar-functions/dwm_battery.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# A dwm_bar function to read the battery level and status
+# Joe Standring <git@joestandring.com>
+# GNU GPLv3
+
+dwm_battery () {
+    # Change BAT1 to whatever your battery is identified as. Typically BAT0 or BAT1
+    #CHARGE=$(cat /sys/class/power_supply/BAT1/capacity)
+    CHARGE=$(acpi | awk '{ print $4 }' | sed 's/.$//')
+    STATUS=$(acpi | awk '{ print $3 }' | sed 's/.$//')
+
+    #STATUS=$(cat /sys/class/power_supply/BAT1/status)
+
+   # printf "%s" "$SEP1"
+   # if [ "$IDENTIFIER" = "unicode" ]; then
+   #     if [ "$STATUS" = "Charging" ]; then
+   #         printf "🔌 %s%% %s" "$CHARGE" "$STATUS"
+   #     else
+   #         printf "🔋 %s%% %s" "$CHARGE" "$STATUS"
+   #     fi
+   # else
+   #     printf "BAT %s%% %s" "$CHARGE" "$STATUS"
+    printf " %s %s" "$CHARGE" "$STATUS"
+   # fi
+    printf "%s\n" "$SEP2"
+}
+
+dwm_battery
+
---
+++ dwm_files/dwm-bar/bar-functions/dwm_pulse.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# A dwm_bar function to show the master volume of PulseAudio
+# Joe Standring <git@joestandring.com>
+# GNU GPLv3
+
+# Dependencies: pamixer
+
+dwm_pulse () {
+    VOL=$(pamixer --get-volume)
+    HEADPHONE=$(cat /proc/asound/card0/codec#0 | grep Pin-ctl | sed -n '7 p' | awk '{ print $3  }')
+
+    printf "%s" "$SEP1"
+
+    if [ "$HEADPHONE" != "OUT" ]; then
+        if [ "$VOL" = "muted" ] || [ "$VOL" -eq 0 ]; then
+            printf " "
+        elif [ "$VOL" -gt 0 ] && [ "$VOL" -le 33 ]; then
+            printf " %s%%" "$VOL"
+        elif [ "$VOL" -gt 33 ] && [ "$VOL" -le 66 ]; then
+            printf " %s%%" "$VOL"
+        else
+            printf " %s%%" "$VOL"
+        fi
+    else
+        if [ "$VOL" = "muted" ] || [ "$VOL" -eq 0 ]; then
+            printf " "
+        elif [ "$VOL" -gt 0 ] && [ "$VOL" -le 33 ]; then
+            printf " %s%%" "$VOL"
+        elif [ "$VOL" -gt 33 ] && [ "$VOL" -le 66 ]; then
+            printf " %s%%" "$VOL"
+        else
+            printf " %s%%" "$VOL"
+        fi
+    fi
+
+    printf "%s\n" "$SEP2"
+}
+
+dwm_pulse
